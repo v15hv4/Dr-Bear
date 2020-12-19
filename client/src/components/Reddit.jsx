5225a6ca15e1f4cfeab558e27ee8e39cb463ef49
v15hv4 <vishva2912@gmail.com>
2020-12-20 04:22:00 +0530

    Introduce Reddit view component

--- client/src/components/Reddit.jsx
+++ client/src/components/Reddit.jsx
@@ -1,7 +1,33 @@
 import { Container } from "reactstrap";
 
+import FadeIn from "react-fade-in";
+
+const RedditItem = ({ comment }) => {
+    return <div className="h5 my-3">"{comment}"</div>;
+};
+
 const Reddit = ({ data }) => {
-    return <Container className="text-light">reddit</Container>;
+    return (
+        <Container tag={FadeIn} className="text-light">
+            <div className="display-4 d-flex align-items-center">
+                Here's what people say on
+                <img src="./Reddit-Logo.wine.svg" alt="reddit" className="reddit-logo ml-n3" />
+                <span className="ml-n4">...</span>
+            </div>
+            <div>
+                {Object.keys(data).map((subreddit, idx) => (
+                    <div className="mb-5" key={idx}>
+                        <div className="h3 mb-4">
+                            <b>r/{subreddit}</b>
+                        </div>
+                        {data[subreddit].map((comment) => (
+                            <RedditItem comment={comment} />
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </Container>
+    );
 };
 
 export default Reddit;
