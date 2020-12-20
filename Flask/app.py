4f8e260e4c1c3a833bc3e00d07eea572f8bd3bfc
Groverkss <groverkss@gmail.com>
2020-12-20 10:54:03 +0530

    Added flask app
    Former-commit-id: 1ba63bfebf4704abd053e7c5eb5d64e8db5918c6

---
+++ Flask/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from pprint import pprint
+from dialog import DialogFlow
+
+app = Flask(__name__)
+dflow = DialogFlow()
+
+
+@app.route("/chat/", methods=["POST"])
+def chat():
+    req_data = request.get_json()
+    input_query = req_data["input"]
+
+    try:
+        flow_output = dflow.collect_intent(input_query)
+    except Exception as error:
+        pprint(error)
+        return "Error"
+
+    pprint(flow_output)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
